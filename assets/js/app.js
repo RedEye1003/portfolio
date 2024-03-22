3711aca0fdb3ec65c89fb7c1a08a303dd9f15e5b
RedEye1003 <hariharasuthan918@gmail.com>
2024-03-23 02:31:00 +0530

    updated linkedin profile

--- assets/js/app.js
+++ assets/js/app.js
@@ -211,7 +211,7 @@ let footer = $(`
     </svg>
     </a>
 
-    <a class="social-button linkedin" href="https://www.linkedin.com/in/harihara-suthan-4411271a4/" target="_blank" >
+    <a class="social-button linkedin" href="https://www.linkedin.com/in/harihara-suthan/" target="_blank" >
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
